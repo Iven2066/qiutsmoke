192fb124da30caba6d45ecc1852c15198fd088ab
Iven2066 <minghao4996@gmail.com>
2025-12-11 16:15:22 +0800

    Update route.ts

--- app/api/quit-date/route.ts
+++ app/api/quit-date/route.ts
@@ -1,4 +1,5 @@
 export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { ensureTables, hasDb } from "@/lib/db";
